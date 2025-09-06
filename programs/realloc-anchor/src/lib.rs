72f7d88bd309d02ce74e0e50321bbb030b997238
Mardiprk <mk1659770@gmail.com>
2025-09-06 04:54:31 +0000

    realloc

--- programs/realloc-anchor/src/lib.rs
+++ programs/realloc-anchor/src/lib.rs
@@ -3,14 +3,60 @@ use anchor_lang::prelude::*;
 declare_id!("F8hAoNSep1xzz3JRbwWrpN6p1wXVNGbMq6EiJQpXnoUS");
 
 #[program]
-pub mod realloc_anchor {
+pub mod note {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, text: String) -> Result<()> {
+        let note = &mut ctx.accounts.note_account;
+        note.owner = ctx.accounts.user.key();
+        note.content = text;
+        msg!(
+            "{} Content has been stored on chain: {}",
+            note.owner,
+            note.content
+        );
+        Ok(())
+    }
+    pub fn append(ctx: Context<Append>, new_text: String) -> Result<()> {
+        let note = &mut ctx.accounts.note_account;
+        note.content.push_str(&new_text);
+        msg!("New Content: {}", note.content);
         Ok(())
     }
 }
 
+#[account]
+pub struct NoteAccount {
+    pub owner: Pubkey,
+    pub content: String,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 32 + 4 + 50,
+        seeds = [b"note", user.key().as_ref()],
+        bump
+    )]
+    pub note_account: Account<'info, NoteAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Append<'info> {
+    #[account(
+        mut,
+        realloc = 8 + 32 + 4 + 200,
+        realloc::payer = user,
+        realloc::zero = false
+    )]
+    pub note_account: Account<'info, NoteAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
--- tests/realloc-anchor.ts
+++ tests/realloc-anchor.ts
@@ -1,16 +1,65 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { ReallocAnchor } from "../target/types/realloc_anchor";
+import { Note } from "../target/types/note";
 
 describe("realloc-anchor", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.reallocAnchor as Program<ReallocAnchor>;
+  const user = anchor.web3.Keypair.generate(); // new test user
+  const program = anchor.workspace.Note as Program<Note>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  let notePda: anchor.web3.PublicKey;
+  let bump: number;
+
+  before(async () => {
+    const airdropSig = await provider.connection.requestAirdrop(
+      user.publicKey,
+      2 * anchor.web3.LAMPORTS_PER_SOL
+    );
+    await provider.connection.confirmTransaction(airdropSig);
+
+    // Derive PDA
+    [notePda, bump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("note"), user.publicKey.toBuffer()],
+      program.programId
+    );
+
+    console.log("User:", user.publicKey.toBase58());
+    console.log("Note PDA:", notePda.toBase58());
+  });
+
+  it("☑️ Initialized!", async () => {
+    const tx = await program.methods
+      .initialize("Hello")
+      .accounts({
+        noteAccount: notePda,
+        user: user.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([user])
+      .rpc();
+
+    console.log("Initialize TX:", tx);
+
+    const account = await program.account.noteAccount.fetch(notePda);
+    console.log("Stored content:", account.content);
+  });
+
+  it("☑️ Append!", async () => {
+    const tx = await program.methods
+      .append(" Solana is very Fast!")
+      .accounts({
+        noteAccount: notePda,
+        user: user.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([user])
+      .rpc();
+
+    console.log("Append TX:", tx);
+
+    const account = await program.account.noteAccount.fetch(notePda);
+    console.log("Updated content:", account.content);
   });
 });
